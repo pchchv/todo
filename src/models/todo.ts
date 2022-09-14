fdda4c5631ab887a2a966aea557eef6982ff6b21
pchchv <ipchchv@gmail.com>
2022-09-14 08:48:50 +0300

    Implement a Todo schema

---
+++ src/models/todo.ts
@@ -0,0 +1,22 @@
+import { Todo } from "@/types/todo"
+import { model, Schema } from "mongoose"
+
+const todoSchema: Schema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    { timestamps: true }
+)
+
+export default model<Todo>("Todo", todoSchema)
