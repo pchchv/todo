7e5d4f401732c20c1efa8be4f8c1931bfd715c0a
pchchv <ipchchv@gmail.com>
2022-09-15 14:37:18 +0300

    Implement a add todos func

--- client/src/API.ts
+++ client/src/API.ts
@@ -9,6 +9,25 @@ export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
         )
         return todos
     } catch (error) {
-        throw new Error(error)
+        throw error
+    }
+}
+
+export const addTodo = async (
+    formData: ITodo
+): Promise<AxiosResponse<ApiDataType>> => {
+    try {
+        const todo: Omit<ITodo, '_id'> = {
+            name: formData.name,
+            description: formData.description,
+            status: false,
+        }
+        const saveTodo: AxiosResponse<ApiDataType> = await axios.post(
+            baseUrl + 'todo',
+            todo
+        )
+        return saveTodo
+    } catch (error) {
+        throw error
     }
 }
