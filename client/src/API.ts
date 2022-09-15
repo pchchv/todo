cf3f8a56585b6e80a346cead10feeb117c296b8f
pchchv <ipchchv@gmail.com>
2022-09-15 14:38:40 +0300

    Implement a update todos func

--- client/src/API.ts
+++ client/src/API.ts
@@ -31,3 +31,20 @@ export const addTodo = async (
         throw error
     }
 }
+
+export const updateTodo = async (
+    todo: ITodo
+  ): Promise<AxiosResponse<ApiDataType>> => {
+    try {
+      const todoUpdate: Pick<ITodo, 'status'> = {
+        status: true,
+      }
+      const updatedTodo: AxiosResponse<ApiDataType> = await axios.put(
+        `${baseUrl}/edit-todo/${todo._id}`,
+        todoUpdate
+      )
+      return updatedTodo
+    } catch (error) {
+      throw error
+    }
+  }
